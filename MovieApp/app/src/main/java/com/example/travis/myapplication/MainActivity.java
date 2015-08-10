d284e0beffc5100addc96505417002a13a846d2d
Travismacmillan <travis.macmillan10@gmail.com>
2015-08-10 17:54:29 -0300

    Added main activity

--- MovieApp/app/src/main/java/com/example/travis/myapplication/MainActivity.java
+++ MovieApp/app/src/main/java/com/example/travis/myapplication/MainActivity.java
@@ -1,12 +1,33 @@
 package com.example.travis.myapplication;
 
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+
+
 
 public class MainActivity extends ActionBarActivity {
+    //http://api.themoviedb.org/3/search/movie?api_key=aaa20ac95777901d30df944cb97e8a54&query=antman
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,4 +56,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
+
